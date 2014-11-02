3f7799c89e86b71f6851475e013928ce7c2c91ef
Tony Han <h.bing612@gmail.com>
2014-11-02 16:27:04 +0800

    handle multi numbers

--- lib/calc.rb
+++ lib/calc.rb
@@ -5,11 +5,19 @@ module Calc
       string.to_i
     elsif string =~ /\A(\d+)(\D)(\d+)(.*)\z/
       left, op, right = $1.to_i, $2, $3.to_i
-      case op
-      when '+' then left + right
-      when '-' then left - right
-      when '*' then left * right
-      when '/' then left / right
+      result =
+        case op
+        when '+' then left + right
+        when '-' then
+          result = left - right
+          result < 0 ? 0 : result
+        when '*' then left * right
+        when '/' then left / right
+        end
+      if $4 == ''
+        result
+      else
+        eval(result.to_s + $4)
       end
     end
   end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -23,6 +23,11 @@ describe 'calc' do
     expect(Calc.eval('11 - 11')).to be_eql 0
   end
 
+  it "should return 0 when minus result is less than 0" do
+    expect(Calc.eval('3-5')).to be_eql 0
+    expect(Calc.eval('4-10')).to be_eql 0
+  end
+
   it "should eval multiply" do
     expect(Calc.eval('2*1')).to be_eql 2
     expect(Calc.eval('2 * 1')).to be_eql 2
@@ -36,5 +41,12 @@ describe 'calc' do
     expect(Calc.eval('4/3')).to be_eql 1
     expect(Calc.eval('4 / 3')).to be_eql 1
     expect(Calc.eval('44 / 33')).to be_eql 1
+  end
+
+  it "hanlde multi numbers" do
+    expect(Calc.eval("1+1+1")).to be_eql 3
+    expect(Calc.eval("1+1+1+1")).to be_eql 4
+    expect(Calc.eval("3*2*2")).to be_eql 12
+    expect(Calc.eval("3*2*2*2")).to be_eql 24
   end
 end
