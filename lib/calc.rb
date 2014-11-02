a77d98228afc1cd3934b135f8afe0036394f7d5e
Tony Han <h.bing612@gmail.com>
2014-11-02 16:34:44 +0800

    split eval_unit out

--- lib/calc.rb
+++ lib/calc.rb
@@ -3,21 +3,26 @@ module Calc
     string = string.gsub(/\s+/, '')
     if string =~ /\A\d+\z/
       string.to_i
-    elsif string =~ /\A(\d+)(\D)(\d+)(.*)\z/
-      left, op, right = $1.to_i, $2, $3.to_i
-      result =
-        case op
-        when '+' then left + right
-        when '-' then
-          result = left - right
-          result < 0 ? 0 : result
-        when '*' then left * right
-        when '/' then left / right
-        end
-      if $4 == ''
+    elsif string =~ /\A(\d+\D?\d+)(.*)\z/
+      result = eval_unit($1)
+      if $2 == ''
         result
       else
-        eval(result.to_s + $4)
+        eval(result.to_s + $2)
+      end
+    end
+  end
+
+  def self.eval_unit(string)
+    if string =~ /\A(\d+)(\D)(\d+)\z/
+      left, op, right = $1.to_i, $2, $3.to_i
+      case op
+      when '+' then left + right
+      when '-' then
+        result = left - right
+        result < 0 ? 0 : result
+      when '*' then left * right
+      when '/' then left / right
       end
     end
   end
