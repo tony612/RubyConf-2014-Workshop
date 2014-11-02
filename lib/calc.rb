a50c847bb0e73fbb2c881fdfe3681d12566af8fa
Tony Han <h.bing612@gmail.com>
2014-11-02 14:58:46 +0800

    finish single number and simple operation tests

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,5 +1,15 @@
 module Calc
   def self.eval(string)
-    2
+    if string =~ /\A\d+\z/
+      string.to_i
+    elsif string =~ /\A(\d+)(\D)(\d+)\z/
+      left, op, right = $1.to_i, $2, $3.to_i
+      case op
+      when '+' then left + right
+      when '-' then left - right
+      when '*' then left * right
+      when '/' then left / right
+      end
+    end
   end
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -4,6 +4,22 @@ require 'calc'
 describe 'calc' do
 
   it 'should eval 2 to 2' do
-    Calc.eval('2') == 2
+    expect(Calc.eval('2')).to be_eql 2
+  end
+
+  it 'should eval 22 to 22' do
+    expect(Calc.eval('22')).to be_eql 22
+  end
+
+  it "should eval plus" do
+    expect(Calc.eval('1+2')).to be_eql 3
+  end
+
+  it "should eval minus" do
+    expect(Calc.eval('1-1')).to be_eql 0
+  end
+
+  it "should eval multiply" do
+    expect(Calc.eval('2*1')).to be_eql 2
   end
 end
