dfebf636191bf0b5d03259d56e162251bd3cb3ea
Tony Han <h.bing612@gmail.com>
2014-11-02 16:26:14 +0800

    use a better way to handle space

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,8 +1,9 @@
 module Calc
   def self.eval(string)
+    string = string.gsub(/\s+/, '')
     if string =~ /\A\d+\z/
       string.to_i
-    elsif string =~ /\A(\d+)\s*(\D)\s*(\d+)\z/
+    elsif string =~ /\A(\d+)(\D)(\d+)(.*)\z/
       left, op, right = $1.to_i, $2, $3.to_i
       case op
       when '+' then left + right
