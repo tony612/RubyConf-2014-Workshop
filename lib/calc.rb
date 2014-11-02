3fefb68d090b5d39df79c6cce4e013e22cab2b88
Tony Han <h.bing612@gmail.com>
2014-11-02 14:59:14 +0800

    consider the spaces in string

--- lib/calc.rb
+++ lib/calc.rb
@@ -2,7 +2,7 @@ module Calc
   def self.eval(string)
     if string =~ /\A\d+\z/
       string.to_i
-    elsif string =~ /\A(\d+)(\D)(\d+)\z/
+    elsif string =~ /\A(\d+)\s*(\D)\s*(\d+)\z/
       left, op, right = $1.to_i, $2, $3.to_i
       case op
       when '+' then left + right
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -13,13 +13,16 @@ describe 'calc' do
 
   it "should eval plus" do
     expect(Calc.eval('1+2')).to be_eql 3
+    expect(Calc.eval('1 + 2')).to be_eql 3
   end
 
   it "should eval minus" do
     expect(Calc.eval('1-1')).to be_eql 0
+    expect(Calc.eval('1 - 1')).to be_eql 0
   end
 
   it "should eval multiply" do
     expect(Calc.eval('2*1')).to be_eql 2
+    expect(Calc.eval('2 * 1')).to be_eql 2
   end
 end
